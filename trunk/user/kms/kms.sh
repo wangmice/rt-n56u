0c9a6e681521bfd327518678f67613690cf4acd2
wangmice <wangmice@126.com>
2017-12-27 06:07:00 +0800

    fix kms server

--- trunk/user/kms/kms.sh
+++ trunk/user/kms/kms.sh
@@ -15,10 +15,10 @@ exit 0
 fi
 #port="1688"
 if [ ! -f /etc_ro/vlmcsd.kmd ]; then
-/etc/storage/vlmcsd&
+/usr/bin/vlmcsd &
 fi
 if [ -f /etc_ro/vlmcsd.kmd ]; then
-/etc/storage/vlmcsd -j /etc_ro/vlmcsd.kmd &
+/usr/bin/vlmcsd -j /etc_ro/vlmcsd.kmd &
 fi
 computer_name=`nvram get computer_name`
 sed -Ei '/_vlmcs._tcp/d' /etc/storage/dnsmasq/dnsmasq.conf
@@ -30,7 +30,7 @@ if [ ! -z "$(ps - w | grep "vlmcsd" | grep -v grep )" ]; then
 logger -t "kms" "启动成功"
 sed -i '/kms/d' /etc/storage/post_wan_script.sh
 cat >> /etc/storage/post_wan_script.sh << EOF
-/usr/bin/kms.sh&
+/usr/bin/kms.sh &
 EOF
 fi
 [ -z "$(ps - w | grep "vlmcsd" | grep -v grep )" ] && logger -t "kms" "启动失败, 注意检查端口是否有冲突" && nvram set kms_enable="0"
